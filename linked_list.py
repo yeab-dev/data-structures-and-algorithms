afa674fa4a6693713f667ed3f653a5fde28b5a8c
yeabsera <yeab.dev@gmail.com>
2024-10-04 23:17:34 +0300

    add: singly linked list implementation

---
+++ linked_list.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next =None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def display(self):
+        currentNode = self.head
+        while currentNode:
+            print(f"{currentNode.data} ->", end="")
+            currentNode = currentNode.next
+    def append(self, data):
+                
+        if not self.head:
+            self.head = Node(data)
+            return
+        lastNode = self.head
+        while lastNode:
+            if lastNode.next == None:
+                lastNode.next = Node(data)
+                return
+            lastNode = lastNode.next
+    
+    def delete(self, data):
+        currentNode = self.head
+        if not currentNode:
+           print("the linked list is empty") 
+        while currentNode and currentNode.next:
+            if currentNode.data == data:
+                self.head = currentNode.next
+                currentNode = None
+            if currentNode.next.data == data:
+                currentNode.next = currentNode.next.next
+            currentNode = currentNode.next
