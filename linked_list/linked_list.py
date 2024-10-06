070eacae9aab28e2a35be980ebd4978d7abfcc18
yeabsera <yeab.dev@gmail.com>
2024-10-06 15:50:20 +0300

    feat: add append and prepend operations

---
+++ linked_list/linked_list.py
@@ -0,0 +1,30 @@
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self.head: Node = None 
+        self.tail: Node = None
+    def append(self, data):
+        newNode = Node(data)
+        if not self.head:
+           self.head, self.tail = newNode
+           newNode.next = newNode
+           newNode.previous = newNode
+           return
+        newNode.next = self.head
+        newNode.previous = self.tail
+        self.head.previous = newNode
+        self.tail.next = newNode
+        self.tail = newNode
+    def prepend(self, data):
+        newNode = Node(data)
+        if not self.head:
+           self.head, self.tail = newNode
+           self.head.previous = self.tail
+           self.tail.next = self.head
+           return
+        newNode.next = self.head
+        newNode.previous = self.tail
+        self.head.previous = newNode
+        self.tail.next = newNode
+        self.head = newNode
