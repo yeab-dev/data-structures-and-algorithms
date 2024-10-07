f8c950fd66645ebd84beb0cdcad40a7b9dd76b49
yeabsera <yeab.dev@gmail.com>
2024-10-07 18:50:01 +0300

    fix: add type annotation for the node properties
    - previous and next were without a type annotation that were solved in this commit

--- linked_list/node.py
+++ linked_list/node.py
@@ -1,5 +1,5 @@
 class Node:
     def __init__(self, data):
-        self.previous = None
-        self.next = None
+        self.previous:Node = None
+        self.next: Node = None
         self.data = data
