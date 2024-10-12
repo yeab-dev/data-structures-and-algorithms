8d9d8f8fb1e4c0e23fba3c10ea01584c7a2efd44
yeabsera <yeab.dev@gmail.com>
2024-10-13 00:28:13 +0300

    feat: add display, isEmpty and size functions

--- stack/linked_list_based_stack/stack.py
+++ stack/linked_list_based_stack/stack.py
@@ -20,3 +20,24 @@ class Stack:
             self.top = temp.next
             self.count -= 1
             return temp.valuet 
+    def peek(self):
+        if not self.top:
+            return None
+        return self.top.value
+    
+    def is_empty(self):
+        return self.top is None
+    def size(self):
+        count = 0
+        current = self.top
+        while current:
+            count += 1
+            current = current.next
+        return count
+    def display(self):
+        elements = []
+        current = self.top
+        while current:
+            elements.append(current.value)
+            current = current.next
+        return elements
