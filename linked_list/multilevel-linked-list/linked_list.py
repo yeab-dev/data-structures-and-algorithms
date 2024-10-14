32e9e53e694d8a42ad26063d82ba425d54f7f050
yeabsera <yeab.dev@gmail.com>
2024-10-14 15:03:11 +0300

    feat: add optional type for head and tail

--- linked_list/multilevel-linked-list/linked_list.py
+++ linked_list/multilevel-linked-list/linked_list.py
@@ -1,5 +1,10 @@
+from typing import Optional
+from node import Node
 class LinkedList:
     def __init__(self):
+        self.head: Optional[Node] = None
+        self.tail: Optional[Node] = None
+
     def append(self, data):
         newNode = Node(data)
         if self.head == None:
