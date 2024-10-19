5fda7f457e8d006c0f18a34978184bae9cdb6cb0
yeabsera <yeab.dev@gmail.com>
2024-10-19 16:19:01 +0300

    ref: restructure the folders
    - created a folder called algorithms for implementing any types of algorithms I learn everyday
    - moved all the data-structure-related files to a new folder called data_structures

--- linked_list/multilevel-linked-list/node.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from linked_list import LinkedList
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next: Optional[Node] = None
-        self.child: Optional[LinkedList] = None
--- linked_list/node.py
+++
@@ -1,5 +0,0 @@
-class Node:
-    def __init__(self, data):
-        self.previous:Node = None
-        self.next: Node = None
-        self.data = data
