37209055a106555bc4572f8da355197a60488a78
yeabsera <yeab.dev@gmail.com>
2024-10-21 23:21:38 +0300

    fix: clear unneeded code out
    Deleted accidently committed changes

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -6,5 +6,3 @@ def quick_sort(nums:List[int]):
     less = [x for x in nums[1:] if x < pivot]
     greater = [x for x in nums[1:] if x > pivot]
     return quick_sort(less) + [pivot] + quick_sort(greater)
-
-print(quick_sort([213,334,54,32,1,54,67,78,9,87,5,34,32,231,54,67,8,988,3,3,213,32]))
