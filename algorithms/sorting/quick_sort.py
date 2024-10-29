f6c8337775db6880db0c61c097e5775517c44479
yeabsera <yeab.dev@gmail.com>
2024-10-30 00:32:57 +0300

    ref: update variable naming

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -4,6 +4,6 @@ def quick_sort(nums:List[int]):
     if len(nums) < 2:
         return nums
     pivot = nums[Random().randint(0, len(nums) - 1)] 
-    less = [x for x in nums if x < pivot]
-    greater = [x for x in nums if x > pivot]
+    left = [x for x in nums if x < pivot]
+    right = [x for x in nums if x > pivot]
     return quick_sort(less) + [pivot] + quick_sort(greater)
