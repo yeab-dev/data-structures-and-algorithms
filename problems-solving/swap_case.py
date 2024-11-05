f9cea3c1f9f00d39365dcf2774b62a6990c1f89a
yeabsera <yeab.dev@gmail.com>
2024-11-05 09:38:09 +0300

    delete duplicate files

--- problems-solving/swap_case.py
+++
@@ -1,8 +0,0 @@
-def swap_case(s):
-    swapped_string = ""
-    for c in s:
-        if (c.isupper()):
-            swapped_string += c.lower()
-        else:
-            swapped_string += c.upper()
-    return swapped_string
