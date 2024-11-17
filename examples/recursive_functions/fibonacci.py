0ce06fa0fe3d7f314521f5b6745508cb234722b5
yeabsera <yeab.dev@gmail.com>
2024-11-17 20:49:08 +0300

    fix example recursive function and solve prob #509

--- examples/recursive_functions/fibonacci.py
+++ examples/recursive_functions/fibonacci.py
@@ -1,5 +1,4 @@
 def fibo(num: int):
     if num == 0: return 0
     elif num == 1: return 1
-    else: fibo(num-2) + fibo(num-1)
-    return fibo(num-2) + fibo(num-1)
+    else:  return fibo(num-2) + fibo(num-1)
