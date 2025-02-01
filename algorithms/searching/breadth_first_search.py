2c6c5003137b7be95a737ac6f613773d2b16346b
yeabsera <yeab.dev@gmail.com>
2025-02-01 20:24:26 +0300

    add a new implementation of an algorithm (BFS)

---
+++ algorithms/searching/breadth_first_search.py
@@ -0,0 +1,23 @@
+# In this algorithm I'll implement the breadth first search algorithm to find the closest mango seller in a network of friends.
+# I'll use a dictionary to represent the network of friends and a queue to keep track of the people to check.
+# for this example I'm gonna assume that whoever's name ends with an 'n' is a mango seller
+from typing import Dict
+from collections import deque
+
+graph:  Dict[str,str] = {
+    "tola": ["gemechu", "bekele", "selam", "dibora", "ermiyas"],
+    "gemechu": ["joe", "john", "betty","fatuma"],
+    "bekele": ["gebeyehu", "misgana", "shimelis"]
+    }
+
+
+def bsd(network: dict) ->str:
+    search_queue = deque()
+    search_queue += network["tola"]
+    while search_queue:
+        person = search_queue.popleft()
+        if person[-1] == "n":
+            return person
+        elif person in network.keys():
+            search_queue += graph[person]
+    return "No one in this network is a mango seller"
