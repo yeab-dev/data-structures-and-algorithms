1531bbc323a2d715381a723ab5ad02efc9dcf668
yeabsera <yeab.dev@gmail.com>
2025-02-05 19:04:20 +0300

    feat: add delete method to the binary tree also renamed binary-tree to binary search tree

--- data_structures/tree/binary-search-tree/tree.py
+++ data_structures/tree/binary-search-tree/tree.py
@@ -36,4 +36,25 @@ class Tree:
                     return
                 else:
                     node = node.left
-    
+    def find_min(self, node: Node):
+        while node.left:
+            node = node.left
+        return node 
+    def delete(self, root: Node, val: Any):
+        if not root:
+            return None
+        if val < root.val:
+            root.left = self.delete(root.left, val)
+        elif val > root.val:
+            root.right = self.delete(root.right, val)
+        else:
+            if not (root.left or root.right):
+                return None
+            elif (root.left is None):
+                return root.right
+            elif root.right is None:
+                return root.left
+            else:
+                successor = self.find_min(root.right)
+                root.val = successor.val
+                root.right = self.delete(root.right, successor.val)
