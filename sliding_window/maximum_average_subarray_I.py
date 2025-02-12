21dbf27c29c2ffe0edf09ac63f5af82872ea9c28
yeabsera <yeab.dev@gmail.com>
2025-02-13 01:09:23 +0300

    fix: fix wrong answer for problem #643

--- sliding_window/maximum_average_subarray_I.py
+++ sliding_window/maximum_average_subarray_I.py
@@ -1,13 +1,12 @@
 from typing import List
+
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        i = 0
-        addition = 0
-        avg = 0.0
-        while i < len(nums) - k+1:
-            addition = sum(nums[i:i+k])
-            if addition / k > avg:
-                avg = addition / k
-            i += 1
-        return avg
-print(Solution().findMaxAverage([-1], 1))
+        addition = sum(nums[:k])
+        max_avg = addition / k 
+
+        for i in range(k, len(nums)):
+            addition += nums[i] - nums[i - k]
+            max_avg = max(max_avg, addition / k)
+
+        return max_avg
